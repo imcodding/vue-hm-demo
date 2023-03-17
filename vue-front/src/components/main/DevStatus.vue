c1f9558b37dcb60ab43eabcb7f66f3b6daf08b1d
sooyun <55366664+imcodding@users.noreply.github.com>
2023-03-17 17:43:17 +0900

    style: grid 크기 조정

--- vue-front/src/components/main/DevStatus.vue
+++ vue-front/src/components/main/DevStatus.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <JqxGrid :width="width" :source="dataAdapter" 
+  <JqxGrid :width="width" :source="dataAdapter" :height="height"
              :columns="columns" :columnsresize="true"
              :pageable="true">
   </JqxGrid>
@@ -16,18 +16,35 @@ export default {
   },
   data: function () {
       return {
-          width: '850',
+          width: '1650',
+          height: 440,
           dataAdapter: new jqx.dataAdapter(this.source),
           columns: [
-              { text: '장비', columngroup: 'ProductDetails', datafield: 'ProductName', width: 250 },
-              { text: 'Quantity per Unit', columngroup: 'ProductDetails', datafield: 'QuantityPerUnit', cellsalign: 'right', align: 'right' },
-              { text: 'Unit Price', columngroup: 'ProductDetails', datafield: 'UnitPrice', align: 'right', cellsalign: 'right', cellsformat: 'c2' },
-              { text: 'Units In Stock', datafield: 'UnitsInStock', cellsalign: 'right', cellsrenderer: this.cellsrenderer, width: 100 },
-              { text: 'Discontinued', columntype: 'checkbox', datafield: 'Discontinued', align: 'center' }
+                    { text : '그룹명', datafield: 'grpName', minwidth : 130, pinned: true },
+                    { text : '장비명', datafield: 'disDevName', minwidth : 150, pinned: true },
+                    { text: '대표IP', datafield: 'devIp', width: 120 },
+                    { text: '타입코드', datafield: 'devKind1', hidden: true},
+                    { text: '종류', datafield: 'devKind2', width: 100, filtertype: 'checkedlist' },
+                    { text: '제조사', datafield: 'vendor', width: 130, filtertype: 'checkedlist' },
+                    { text: '모델', datafield: 'model', width: 150, filtertype: 'checkedlist' },
+                    { text: '최근부팅', datafield: 'upTime', width: 120, cellsalign: 'center'},
+                    {
+                        text: '부팅후',
+                        datafield: 'upTimeSumSec',
+                        width: 150,
+                        filtertype: 'number'
+                    },
+                    { text: '장비위치', datafield: 'devLocation', width: 150, hidden: true  },
+                    { text: "회선수", datafield: "ifCnt", width: 80, cellsformat: "n", cellsalign: "right", filtertype: "number" },
+                    { text: "Alive 수", datafield: "aliveIfCnt", width: 80, cellsformat: "n", cellsalign: "right", filtertype: "number" },
+                    { text: "Dead 수", datafield: "deadIfCnt", width: 80, cellsformat: "n", cellsalign: "right", filtertype: "number" , hidden: true},
+                    { text: "메모리", datafield: "memory", width: 100 },
+                    { text: "CPU사용률", datafield: "cpuPer", width: 100, cellsformat: "d", cellsalign: "right", filtertype: "number" },
+                    { text: "메모리사용률", datafield: "memPer", width: 110, cellsformat: "d", cellsalign: "right", filtertype: "number" },
+                    { text: '시리얼', datafield: 'machine_serial', width: 130 },
+                    { text: 'OS버전', datafield: 'machineVer', width: 130 },
+                    { text: "커뮤니티", datafield: "isCommunity", width: 80, hidden:true }
           ],
-          columngroups: [
-              { text: 'Product Details', align: 'center', name: 'ProductDetails' }
-          ]
       }
   },
   beforeCreate: function () {
@@ -40,10 +57,6 @@ export default {
               { name: 'UnitsInStock', type: 'float' },
               { name: 'Discontinued', type: 'bool' }
           ],
-          root: 'Products',
-          record: 'Product',
-          id: 'ProductID',
-          url: 'sampledata/products.xml'
       };
   },
   methods: {
